6e8a29c38de8cd7b0bf218488df6560e0b8e8cf5
Hector Huertas <hectorhuertas@gmail.com>
2019-10-14 14:21:35 +0200

    Sleep 1/3 of the duration, to allow for failed renewals

--- sleep.go
+++ sleep.go
@@ -5,8 +5,8 @@ import (
 	"time"
 )
 
-// Sleep for 2/3 of the lease duration with a random jitter to discourage syncronised API calls from
+// Sleep for 1/3 of the lease duration with a random jitter to discourage syncronised API calls from
 // multiple instances of the application
 func sleepDuration(leaseDuration time.Duration, rand *rand.Rand) time.Duration {
-	return time.Duration((float64(leaseDuration.Nanoseconds()) * 2 / 3) * (rand.Float64() + 1.50) / 2)
+	return time.Duration((float64(leaseDuration.Nanoseconds()) * 1 / 3) * (rand.Float64() + 1.50) / 2)
 }
