c2f898e73156644388e2e6726e2372beaa4749cf
Rob Best <robertbest89@gmail.com>
2020-08-20 11:59:39 +0100

    sidecar: add exponential backoff
    Log errors rather than exiting out and apply an exponential backoff to
    avoid nuking the vault server with requests.

--- sidecar/credentials.go
+++ sidecar/credentials.go
@@ -1,10 +1,13 @@
 package sidecar
 
 import (
-	vault "github.com/hashicorp/vault/api"
 	"io/ioutil"
+	"log"
 	"math/rand"
 	"time"
+
+	"github.com/cenkalti/backoff"
+	vault "github.com/hashicorp/vault/api"
 )
 
 // credentialsRenewer renews the credentials
@@ -16,6 +19,7 @@ type credentialsRenewer struct {
 	providerConfig ProviderConfig
 	tokenPath      string
 	vaultConfig    *vault.Config
+	vaultClient    *vault.Client
 }
 
 // start the renewer
@@ -26,35 +30,38 @@ func (cr *credentialsRenewer) start() {
 		cr.errors <- err
 		return
 	}
+	cr.vaultClient = client
+
+	b := backoff.NewExponentialBackOff()
+	b.RandomizationFactor = 0.2
+	b.Multiplier = 2
+	b.InitialInterval = 2 * time.Second
+	b.MaxElapsedTime = 0
 
 	for {
-		// Reload vault configuration from the environment
-		if err := cr.vaultConfig.ReadEnvironment(); err != nil {
+		var (
+			creds    interface{}
+			duration time.Duration
+			err      error
+		)
+
+		err = backoff.RetryNotify(
+			func() error {
+				creds, duration, err = cr.renew()
+
+				return err
+			},
+			b,
+			func(err error, t time.Duration) {
+				log.Printf("error: %s, backoff: %v", err, t)
+			},
+		)
+		if err != nil {
 			cr.errors <- err
 			return
 		}
 
-		// Login into Vault via kube SA
-		jwt, err := ioutil.ReadFile(cr.tokenPath)
-		if err != nil {
-			cr.errors <- err
-			return
-		}
-		secret, err := client.Logical().Write("auth/"+cr.kubePath+"/login", map[string]interface{}{
-			"jwt":  string(jwt),
-			"role": cr.kubeRole,
-		})
-		if err != nil {
-			cr.errors <- err
-			return
-		}
-		client.SetToken(secret.Auth.ClientToken)
-
-		creds, duration, err := cr.providerConfig.credentials(client)
-		if err != nil {
-			cr.errors <- err
-			return
-		}
+		// Feed the credentials through the channel
 		cr.credentials <- creds
 
 		// Used to generate random values for sleeping between renewals
@@ -63,4 +70,29 @@ func (cr *credentialsRenewer) start() {
 		// Sleep until its time to renew the creds
 		time.Sleep(sleepDuration(duration, random))
 	}
+}
+
+// renew the credentials
+func (cr *credentialsRenewer) renew() (interface{}, time.Duration, error) {
+	// Reload vault configuration from the environment
+	if err := cr.vaultConfig.ReadEnvironment(); err != nil {
+		return nil, -1, err
+	}
+
+	// Login to Vault via kube SA
+	jwt, err := ioutil.ReadFile(cr.tokenPath)
+	if err != nil {
+		return nil, -1, err
+	}
+	secret, err := cr.vaultClient.Logical().Write("auth/"+cr.kubePath+"/login", map[string]interface{}{
+		"jwt":  string(jwt),
+		"role": cr.kubeRole,
+	})
+	if err != nil {
+		return nil, -1, err
+	}
+	cr.vaultClient.SetToken(secret.Auth.ClientToken)
+
+	// Retrieve credentials for the provider
+	return cr.providerConfig.credentials(cr.vaultClient)
 }
